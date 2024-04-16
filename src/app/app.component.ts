ac95a50230b00c40db34961eba43c56334a571eb
Vinicius Sironi <viniciussironi@gmail.com>
2024-04-15 21:53:35 -0300

    novidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { NewsComponent } from './components/news/news.component';
+import { HelpComponent } from './components/help/help.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, CardsComponent],
+  imports: [HeaderComponent, CardsComponent, NewsComponent, HelpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
